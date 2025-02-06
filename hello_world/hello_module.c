0e8cd167282e391adc86920957ef2038bc062aa2
sj608 <steven_psj@hotmail.com>
2025-02-05 21:55:19 -0800

    module that throws oops

--- hello_world/hello_module.c
+++ hello_world/hello_module.c
@@ -3,10 +3,23 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
+#define OP_READ     0
+#define OP_WRITE    1
+#define OP_OOPS     OP_WRITE
+
 // Removed after initialization (static kerenl or module)
 static int __init hello_init(void)
 {
-    pr_debug("Hello World\n");
+    int *a;
+    a = (int*) 0x00001234;
+#if OP_OOPS == OP_WRITE
+    *a = 3;
+#elif OP_OOPS == OP_READ
+    printk(KERN_ALERT "value = %d\n", *a);
+#else
+#error "Unknown op for oops"
+#endif
+
     return 0;
 }
 
